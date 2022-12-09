9eba48b529ebf73304ad75e79d504f92960bbe3b
krishnenthu <krishnaa4175@gmail.com>
2022-12-09 08:04:41 +0530

    adding selenium commands

--- src/test/java/seleniumcommands/BasicCommands.java
+++ src/test/java/seleniumcommands/BasicCommands.java
@@ -8,31 +8,31 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class BasicCommands {
 
     public static void main(String[] args) {
-
-       /*
+        WebDriver driver;
+       /**
          String driverPath=System.getProperty("user.dir");
          System.setProperty("webdriver.chrome.driver","C:\\Users\\KRISHNENTHU\\IdeaProjects\\seleniumbasics\\src\\test\\resources\\driverfiles");
-        */
+         System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\src\\test\\resources\\driverfiles");
+        **/
 
-        /* Launch Chrome Browser */
+        /** Launching Chrome Browser **/
 
-        WebDriver driver = new ChromeDriver();      //To launch the browser
+        driver = new ChromeDriver();      //To launch the browser
         driver.manage().window().maximize();        // To maximize the browser
         driver.get("https://www.obsqurazone.com");
         //driver.close();                             //To close the browser
 
-        /* Launch Edge Browser */
+        /** Launch Edge Browser **/
 
-        WebDriver driver1= new EdgeDriver();
-        driver1.manage().window().maximize();
-        driver1.get("https://www.obsqurazone.com");
+        driver = new EdgeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.obsqurazone.com");
 
-        /* Launch Firefox Browser */
+        /** Launch Firefox Browser **/
 
-        WebDriver driver2= new FirefoxDriver();
-        driver2.manage().window().maximize();
-        driver2.get("https://www.obsqurazone.com");
-
+        driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.obsqurazone.com");
 
     }
 }
