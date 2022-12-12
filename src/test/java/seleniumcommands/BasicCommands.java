3c1c6e7e46b1e96974518c39e68cc8a0beee1e9b
krishnenthu <krishnaa4175@gmail.com>
2022-12-12 06:43:34 +0530

    adding the selenium commands

--- src/test/java/seleniumcommands/BasicCommands.java
+++ src/test/java/seleniumcommands/BasicCommands.java
@@ -9,30 +9,37 @@ public class BasicCommands {
 
     public static void main(String[] args) {
         WebDriver driver;
-       /**
+        /**  setting the path explicitly
+         System.setProperty("webdriver.chrome.driver","C:\\Users\\KRISHNENTHU\\IdeaProjects\\seleniumbasics\\src\\test\\resources\\driverfiles\\chromedriver.exe");
          String driverPath=System.getProperty("user.dir");
-         System.setProperty("webdriver.chrome.driver","C:\\Users\\KRISHNENTHU\\IdeaProjects\\seleniumbasics\\src\\test\\resources\\driverfiles");
-         System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\src\\test\\resources\\driverfiles");
-        **/
+         System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\src\\test\\resources\\driverfiles\\chromedriver.exe");
+         **/
 
         /** Launching Chrome Browser **/
+        // System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\src\\test\\resources\\driverfiles\\chromedriver.exe");
+        driver = new ChromeDriver();                /** To launch the browser **/
+        driver.manage().window().maximize();        /** To maximize the browser **/
+        driver.manage().deleteAllCookies();         /** to delete the cookies **/
+        driver.get("https://selenium.obsqurazone.com/index.php");
+        String currentUrl = driver.getCurrentUrl();   /** to get the url **/
+        System.out.println(currentUrl);
+        String title= driver.getTitle();             /** to get the title **/
+        System.out.println(title);
+        String sourceCode= driver.getPageSource();   /** to get the source code **/
+        System.out.println(sourceCode);
+        driver.close();                          /** To close the browser **/
 
-        driver = new ChromeDriver();      //To launch the browser
-        driver.manage().window().maximize();        // To maximize the browser
-        driver.get("https://www.obsqurazone.com");
-        //driver.close();                             //To close the browser
+//        /** Launch Edge Browser **/
+//        driver = new EdgeDriver();
+//        driver.manage().window().maximize();
+//        driver.get("https://selenium.obsqurazone.com/index.php");
+//        driver.close();
 
-        /** Launch Edge Browser **/
-
-        driver = new EdgeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://www.obsqurazone.com");
-
-        /** Launch Firefox Browser **/
-
-        driver = new FirefoxDriver();
-        driver.manage().window().maximize();
-        driver.get("https://www.obsqurazone.com");
+//        /** Launch Firefox Browser **/
+//        driver = new FirefoxDriver();
+//        driver.manage().window().maximize();
+//        driver.get("https://selenium.obsqurazone.com/index.php");
+//         driver.close();
 
     }
 }
