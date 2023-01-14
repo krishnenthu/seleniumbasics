cf694c94986d6bd800c533d4037bd9f8b9d7b851
krishnenthu <krishnaa4175@gmail.com>
2023-01-14 09:56:42 +0530

    adding vehicle class

--- src/test/java/seleniumbasics/Vehicle.java
+++ src/test/java/seleniumbasics/Vehicle.java
@@ -1,2 +1,14 @@
-package seleniumbasics;public class Vehicle {
+package seleniumbasics;
+
+import org.testng.annotations.Test;
+
+public class Vehicle {
+    @Test
+    public void verifyVehicleQuality(){
+        System.out.println("Vehicle quality is good");
+    }
+    @Test
+    public void verifyvehicleBrand(){
+        System.out.println("Vehicle Brand");
+    }
 }
