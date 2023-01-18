36337b59c09eeab52f644ec39f7d300a523d2140
krishnenthu <krishnaa4175@gmail.com>
2023-01-18 13:11:13 +0530

    Adding DemoWebshop with homework

--- src/test/java/seleniumbasics/DemoWebShop.java
+++ src/test/java/seleniumbasics/DemoWebShop.java
@@ -17,6 +17,7 @@ import java.util.List;
 public class DemoWebShop {
 
     WebDriver driver;
+
     public void testInitialize(String browser) {
         if (browser.equals("chrome")) {
             driver = new ChromeDriver();
@@ -42,6 +43,7 @@ public class DemoWebShop {
         testInitialize(browserName);
         driver.get(url);
     }
+
     @AfterMethod
     public void tearDown(ITestResult result) throws IOException {
         if (result.getStatus() == ITestResult.FAILURE) {
@@ -51,12 +53,14 @@ public class DemoWebShop {
         }
         driver.close();
     }
+
     @Test
     public void TC_001_verifyDemoWebShopTitle() {
         String actualTitle = driver.getTitle();
         String expectedTitle = "Demo Web Shop";
         Assert.assertEquals(actualTitle, expectedTitle, "Invalid Title found");
     }
+
     @Test
     public void TC_002_verifyLogin() {
         WebElement login = driver.findElement(By.xpath("//a[@class='ico-login']"));
@@ -72,6 +76,7 @@ public class DemoWebShop {
         String actualEmail = userAccount.getText();
         Assert.assertEquals(emailid, actualEmail, "Login failed");
     }
+
     @Test
     public void TC_003_verifyRegistration() {
         WebElement registerlink = driver.findElement(By.xpath("//a[text()='Register']"));
@@ -95,6 +100,7 @@ public class DemoWebShop {
         String actualEmail = userAccount.getText();
         Assert.assertEquals(actualEmail, email, "Registration failed");
     }
+
     public void selectGender(List<WebElement> genders, String gen) {
         for (int i = 0; i < genders.size(); i++) {
             String gender = genders.get(i).getText();
@@ -103,8 +109,9 @@ public class DemoWebShop {
             }
         }
     }
+
     @Test
-    public void TC_004_verifyDemoWebShopTitleHomeWork() throws IOException {
+    public void TC_004_verifyDemoWebShopTitleWithExcelHomeWork() throws IOException {
         String actualTitle = driver.getTitle();
         List<ArrayList<String>> data = ExcelUtility.excelDataReader("\\src\\test\\resources\\TestData.xlsx", "HomePage");
         String expectedTitle = data.get(0).get(1);
@@ -112,11 +119,11 @@ public class DemoWebShop {
     }
 
     @Test(dataProvider = "Credentials")
-    public void TC_005_verifyInvalidLogin(String userName, String pword){
-        WebElement login1= driver.findElement(By.xpath("//a[@class='ico-login']"));
+    public void TC_005_verifyInvalidLoginWithDataProvider(String userName, String pword) {
+        WebElement login1 = driver.findElement(By.xpath("//a[@class='ico-login']"));
         login1.click();
         //String mail = userName;
-        WebElement email= driver.findElement(By.xpath("//input[@id='Email']"));
+        WebElement email = driver.findElement(By.xpath("//input[@id='Email']"));
         //email.sendKeys(mail);
         email.sendKeys(userName);
         WebElement password = driver.findElement(By.xpath("//input[@id='Password']"));
@@ -124,48 +131,49 @@ public class DemoWebShop {
         WebElement loginButton = driver.findElement(By.xpath("//input[@class='button-1 login-button']"));
         loginButton.click();
         WebElement error = driver.findElement(By.xpath("//div[@class='validation-summary-errors']//span"));
-        String actMsg=error.getText();
-        String expMsg= "Login was unsuccessful. Please correct the errors and try again.";
-        Assert.assertEquals(actMsg,expMsg,"error message not found");
+        String actMsg = error.getText();
+        String expMsg = "Login was unsuccessful. Please correct the errors and try again.";
+        Assert.assertEquals(actMsg, expMsg, "error message not found");
     }
+
     @DataProvider(name = "Credentials")
-    public Object[][] userCredentials(){
-        Object[][] data={{"priyagopaltestn@gmail.com", "test13"},{"gopaltest@gmail.com", "test123"},{"gopaltest@gmail.com", "test3"}};
-        return  data;
+    public Object[][] userCredentials() {
+        Object[][] data = {{"priyagopaltestn@gmail.com", "test13"}, {"gopaltest@gmail.com", "test123"}, {"gopaltest@gmail.com", "test3"}};
+        return data;
     }
 
     @Test
-    public void TC_006_verifyRegistrationDataRandomGeneration(){
-        WebElement reg1= driver.findElement(By.xpath("//a[@class='ico-register']"));
+    public void TC_006_verifyRegistrationDataRandomGeneration() {
+        WebElement reg1 = driver.findElement(By.xpath("//a[@class='ico-register']"));
         reg1.click();
-        String fName= RandomDataUtility.getfName();
+        String fName = RandomDataUtility.getfName();
         String lName = RandomDataUtility.getlName();
-        String email= RandomDataUtility.getRandomEmail();
-        String pWord = fName+"@123";
-        List<WebElement> gender= driver.findElements(By.xpath("//input[@name='Gender']"));
+        String email = RandomDataUtility.getRandomEmail();
+        String pWord = fName + "@123";
+        List<WebElement> gender = driver.findElements(By.xpath("//input[@name='Gender']"));
         selectGender(gender, "M");
-        WebElement firstName=driver.findElement(By.id("FirstName"));
+        WebElement firstName = driver.findElement(By.id("FirstName"));
         firstName.sendKeys(fName);
-        WebElement lastName=driver.findElement(By.id("LastName"));
+        WebElement lastName = driver.findElement(By.id("LastName"));
         lastName.sendKeys(lName);
-        WebElement emailField=driver.findElement(By.id("Email"));
+        WebElement emailField = driver.findElement(By.id("Email"));
         emailField.sendKeys(email);
-        WebElement passwordField=driver.findElement(By.id("Password"));
+        WebElement passwordField = driver.findElement(By.id("Password"));
         passwordField.sendKeys(pWord);
-        WebElement passwordConfirm=driver.findElement(By.id("ConfirmPassword"));
+        WebElement passwordConfirm = driver.findElement(By.id("ConfirmPassword"));
         passwordConfirm.sendKeys(pWord);
-        WebElement register=driver.findElement(By.id("register-button"));
+        WebElement register = driver.findElement(By.id("register-button"));
         register.click();
-        WebElement userAccount=driver.findElement(By.xpath("//div[@class='header-links']//a[@class='account']"));
-        String actualEmail=userAccount.getText();
-        Assert.assertEquals(actualEmail,email,"Registration failed");
+        WebElement userAccount = driver.findElement(By.xpath("//div[@class='header-links']//a[@class='account']"));
+        String actualEmail = userAccount.getText();
+        Assert.assertEquals(actualEmail, email, "Registration failed");
     }
 
     @Test(dataProvider = "ValidCredentials")
-    public void TC_007_verifyValidLoginHomework(String userName, String pword){
-        WebElement login= driver.findElement(By.xpath("//a[@class='ico-login']"));
+    public void TC_007_verifyValidLoginWithDataProviderHomework(String userName, String pword) {
+        WebElement login = driver.findElement(By.xpath("//a[@class='ico-login']"));
         login.click();
-        WebElement email= driver.findElement(By.xpath("//input[@id='Email']"));
+        WebElement email = driver.findElement(By.xpath("//input[@id='Email']"));
         email.sendKeys(userName);
         WebElement password = driver.findElement(By.xpath("//input[@id='Password']"));
         password.sendKeys(pword);
@@ -173,13 +181,63 @@ public class DemoWebShop {
         loginButton.click();
         WebElement userAccount = driver.findElement(By.xpath("//div[@class='header-links']//a[@class='account']"));
         String actualEmail = userAccount.getText();
-        Assert.assertEquals(actualEmail,userName,"Login failed");
+        Assert.assertEquals(actualEmail, userName, "Login failed");
     }
+
     @DataProvider(name = "ValidCredentials")
-    public Object[][] validUserCredentials(){
-        Object[][] data={{"priyagopaltestn@gmail.com", "test123"},{"krishnatest123@gmail.com","test@123"}};
-        return  data;
+    public Object[][] validUserCredentials() {
+        Object[][] data = {{"priyagopaltestn@gmail.com", "test123"}, {"krishnatest123@gmail.com", "test@123"}};
+        return data;
     }
 
+    @Test
+    @Parameters({"username_email", "pass_password"})
+    public void TC_008_verifyValidLoginWithParameterHomework(String username, String password) {
+        WebElement login = driver.findElement(By.xpath("//a[@class='ico-login']"));
+        login.click();
+        WebElement emailField = driver.findElement(By.xpath("//input[@id='Email']"));
+        emailField.sendKeys(username);
+        WebElement passwordField = driver.findElement(By.xpath("//input[@id='Password']"));
+        passwordField.sendKeys(password);
+        WebElement loginButton = driver.findElement(By.xpath("//input[@class='button-1 login-button']"));
+        loginButton.click();
+        WebElement userAccount = driver.findElement(By.xpath("//div[@class='header-links']//a[@class='account']"));
+        String actualEmail = userAccount.getText();
+        Assert.assertEquals(actualEmail, username, "Login failed");
+    }
 
+    @Test
+    public void TC_009_verifyRegistrationWithExcelAndRandomDataGeneration() throws IOException {
+        WebElement registerLink = driver.findElement(By.xpath("//a[@class='ico-register']"));
+        registerLink.click();
+        List<ArrayList<String>> data = ExcelUtility.excelDataReader("\\src\\test\\resources\\TestData.xlsx", "RegistrationPage");
+        String genderData=data.get(0).get(1);
+        String fName = data.get(1).get(1);
+        String lName = data.get(2).get(1);
+        String password = data.get(3).get(1);
+        String confirmPassword = data.get(4).get(1);
+        String email=RandomDataUtility.getRandomEmail();
+        if (genderData.equals("Male")){
+            WebElement gender=driver.findElement(By.id("gender-male"));
+            gender.click();
+        }else {
+            WebElement gender=driver.findElement(By.id("gender-female"));
+            gender.click();
+        }
+        WebElement firstName = driver.findElement(By.id("FirstName"));
+        firstName.sendKeys(fName);
+        WebElement lastName = driver.findElement(By.id("LastName"));
+        lastName.sendKeys(lName);
+        WebElement emailField = driver.findElement(By.id("Email"));
+        emailField.sendKeys(email);
+        WebElement passwordField = driver.findElement(By.id("Password"));
+        passwordField.sendKeys(password);
+        WebElement passwordConfirm = driver.findElement(By.id("ConfirmPassword"));
+        passwordConfirm.sendKeys(confirmPassword);
+        WebElement register = driver.findElement(By.id("register-button"));
+        register.click();
+        WebElement userAccount = driver.findElement(By.xpath("//div[@class='header-links']//a[@class='account']"));
+        String actualEmail = userAccount.getText();
+        Assert.assertEquals(actualEmail, email, "Registration failed");
+    }
 }
